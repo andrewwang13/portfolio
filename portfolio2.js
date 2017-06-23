9c9123fa9062704df326ef225d354576b4323481
andrewwang13 <awang@nerdwallet.com>
2017-06-23 07:59:03 -0700

    added galleries
    events
    outdoors

--- portfolio2.js
+++ portfolio2.js
@@ -4,89 +4,89 @@ $(document).ready(function() {
 
 	$(".left_nav").click(function(){
 
- 		if($(".la_1").css("opacity")==1){ // the .css(XXX) lets js talk to css
- 			$(".la_1").fadeTo(fadeTime,0);
-	  		$(".la_10").fadeTo(fadeTime,1);
+ 		if($(".image_1").css("opacity")==1){ // the .css(XXX) lets js talk to css
+ 			$(".image_1").fadeTo(fadeTime,0);
+	  		$(".image_10").fadeTo(fadeTime,1);
 		} 
-		if($(".la_2").css("opacity")==1) {
-		  	$(".la_2").fadeTo(fadeTime,0);
-		  	$(".la_1").fadeTo(fadeTime,1);
+		if($(".image_2").css("opacity")==1) {
+		  	$(".image_2").fadeTo(fadeTime,0);
+		  	$(".image_1").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_3").css("opacity")==1) {
-		  	$(".la_3").fadeTo(fadeTime,0);
-		  	$(".la_2").fadeTo(fadeTime,1);
+	  	if($(".image_3").css("opacity")==1) {
+		  	$(".image_3").fadeTo(fadeTime,0);
+		  	$(".image_2").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_4").css("opacity")==1) {
-		  	$(".la_4").fadeTo(fadeTime,0);
-		  	$(".la_3").fadeTo(fadeTime,1);
+	  	if($(".image_4").css("opacity")==1) {
+		  	$(".image_4").fadeTo(fadeTime,0);
+		  	$(".image_3").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_5").css("opacity")==1) {
-		  	$(".la_5").fadeTo(fadeTime,0);
-		  	$(".la_4").fadeTo(fadeTime,1);
+	  	if($(".image_5").css("opacity")==1) {
+		  	$(".image_5").fadeTo(fadeTime,0);
+		  	$(".image_4").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_6").css("opacity")==1) {
-		  	$(".la_6").fadeTo(fadeTime,0);
-		  	$(".la_5").fadeTo(fadeTime,1);
+	  	if($(".image_6").css("opacity")==1) {
+		  	$(".image_6").fadeTo(fadeTime,0);
+		  	$(".image_5").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_7").css("opacity")==1) {
-		  	$(".la_7").fadeTo(fadeTime,0);
-		  	$(".la_6").fadeTo(fadeTime,1);
+	  	if($(".image_7").css("opacity")==1) {
+		  	$(".image_7").fadeTo(fadeTime,0);
+		  	$(".image_6").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_8").css("opacity")==1) {
-		  	$(".la_8").fadeTo(fadeTime,0);
-		  	$(".la_7").fadeTo(fadeTime,1);
+	  	if($(".image_8").css("opacity")==1) {
+		  	$(".image_8").fadeTo(fadeTime,0);
+		  	$(".image_7").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_9").css("opacity")==1) {
-		  	$(".la_9").fadeTo(fadeTime,0);
-		  	$(".la_8").fadeTo(fadeTime,1);
+	  	if($(".image_9").css("opacity")==1) {
+		  	$(".image_9").fadeTo(fadeTime,0);
+		  	$(".image_8").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_10").css("opacity")==1) {
-		  	$(".la_10").fadeTo(fadeTime,0);
-		  	$(".la_9").fadeTo(fadeTime,1);
+	  	if($(".image_10").css("opacity")==1) {
+		  	$(".image_10").fadeTo(fadeTime,0);
+		  	$(".image_9").fadeTo(fadeTime,1);
 	  	}
 	});  	
 
 	$(".right_nav").click(function(){
 
- 		if($(".la_1").css("opacity")==1){ // the .css(XXX) lets js talk to css
- 			$(".la_1").fadeTo(fadeTime,0);
-	  		$(".la_2").fadeTo(fadeTime,1);
+ 		if($(".image_1").css("opacity")==1){ // the .css(XXX) lets js talk to css
+ 			$(".image_1").fadeTo(fadeTime,0);
+	  		$(".image_2").fadeTo(fadeTime,1);
 		} 
-		if($(".la_2").css("opacity")==1) {
-		  	$(".la_2").fadeTo(fadeTime,0);
-		  	$(".la_3").fadeTo(fadeTime,1);
+		if($(".image_2").css("opacity")==1) {
+		  	$(".image_2").fadeTo(fadeTime,0);
+		  	$(".image_3").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_3").css("opacity")==1) {
-		  	$(".la_3").fadeTo(fadeTime,0);
-		  	$(".la_4").fadeTo(fadeTime,1);
+	  	if($(".image_3").css("opacity")==1) {
+		  	$(".image_3").fadeTo(fadeTime,0);
+		  	$(".image_4").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_4").css("opacity")==1) {
-		  	$(".la_4").fadeTo(fadeTime,0);
-		  	$(".la_5").fadeTo(fadeTime,1);
+	  	if($(".image_4").css("opacity")==1) {
+		  	$(".image_4").fadeTo(fadeTime,0);
+		  	$(".image_5").fadeTo(fadeTime,1);
 	  	}
-	  	if($(".la_5").css("opacity")==1) {
-		  	$(".la_5").fadeTo(fadeTime,0);
-		  	$(".la_6").fadeTo(fadeTime,1);
+	  	if($(".image_5").css("opacity")==1) {
+		  	$(".image_5").fadeTo(fadeTime,0);
+		  	$(".image_6").fadeTo(fadeTime,1);
 	  	}  
-	  	if($(".la_6").css("opacity")==1) {
-		  	$(".la_6").fadeTo(fadeTime,0);
-		  	$(".la_7").fadeTo(fadeTime,1);
+	  	if($(".image_6").css("opacity")==1) {
+		  	$(".image_6").fadeTo(fadeTime,0);
+		  	$(".image_7").fadeTo(fadeTime,1);
 	  	} 
-	  	if($(".la_7").css("opacity")==1) {
-		  	$(".la_7").fadeTo(fadeTime,0);
-		  	$(".la_8").fadeTo(fadeTime,1);
+	  	if($(".image_7").css("opacity")==1) {
+		  	$(".image_7").fadeTo(fadeTime,0);
+		  	$(".image_8").fadeTo(fadeTime,1);
 	  	}  	
-	  	if($(".la_8").css("opacity")==1) {
-		  	$(".la_8").fadeTo(fadeTime,0);
-		  	$(".la_9").fadeTo(fadeTime,1);
+	  	if($(".image_8").css("opacity")==1) {
+		  	$(".image_8").fadeTo(fadeTime,0);
+		  	$(".image_9").fadeTo(fadeTime,1);
 	  	}  	
-	   	if($(".la_9").css("opacity")==1) {
-		  	$(".la_9").fadeTo(fadeTime,0);
-		  	$(".la_10").fadeTo(fadeTime,1);
+	   	if($(".image_9").css("opacity")==1) {
+		  	$(".image_9").fadeTo(fadeTime,0);
+		  	$(".image_10").fadeTo(fadeTime,1);
 	  	}  	
-	   	if($(".la_10").css("opacity")==1) {
-		  	$(".la_10").fadeTo(fadeTime,0);
-		  	$(".la_1").fadeTo(fadeTime,1);
+	   	if($(".image_10").css("opacity")==1) {
+		  	$(".image_10").fadeTo(fadeTime,0);
+		  	$(".image_1").fadeTo(fadeTime,1);
 	  	}  	
 	});
 		
