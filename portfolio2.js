0f84d59fae2d5f04d16cdbf2ed07c1032b251312
andrewwang13 <awang@nerdwallet.com>
2017-07-01 13:39:42 -0700

    added sections, downsized some photos

--- portfolio2.js
+++ portfolio2.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
+// cycling left and rightß
 	var fadeTime = 1000;
-
 	$(".left_nav").click(function(){
 
  		if($(".image_1").css("opacity")==1){ // the .css(XXX) lets js talk to css
@@ -89,6 +89,99 @@ $(document).ready(function() {
 		  	$(".image_1").fadeTo(fadeTime,1);
 	  	}  	
 	});
+
+//left and right key behavior
+$("body").keydown(function(e){
+    // left arrow
+    if ((e.keyCode || e.which) == 37)
+    {   
+		if($(".image_1").css("opacity")==1){ // the .css(XXX) lets js talk to css
+ 			$(".image_1").fadeTo(fadeTime,0);
+	  		$(".image_10").fadeTo(fadeTime,1);
+		} 
+		if($(".image_2").css("opacity")==1) {
+		  	$(".image_2").fadeTo(fadeTime,0);
+		  	$(".image_1").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_3").css("opacity")==1) {
+		  	$(".image_3").fadeTo(fadeTime,0);
+		  	$(".image_2").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_4").css("opacity")==1) {
+		  	$(".image_4").fadeTo(fadeTime,0);
+		  	$(".image_3").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_5").css("opacity")==1) {
+		  	$(".image_5").fadeTo(fadeTime,0);
+		  	$(".image_4").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_6").css("opacity")==1) {
+		  	$(".image_6").fadeTo(fadeTime,0);
+		  	$(".image_5").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_7").css("opacity")==1) {
+		  	$(".image_7").fadeTo(fadeTime,0);
+		  	$(".image_6").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_8").css("opacity")==1) {
+		  	$(".image_8").fadeTo(fadeTime,0);
+		  	$(".image_7").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_9").css("opacity")==1) {
+		  	$(".image_9").fadeTo(fadeTime,0);
+		  	$(".image_8").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_10").css("opacity")==1) {
+		  	$(".image_10").fadeTo(fadeTime,0);
+		  	$(".image_9").fadeTo(fadeTime,1);
+	  	}        
+    }
+    // right arrow
+    if ((e.keyCode || e.which) == 39)
+    {
+        if($(".image_1").css("opacity")==1){ // the .css(XXX) lets js talk to css
+ 			$(".image_1").fadeTo(fadeTime,0);
+	  		$(".image_2").fadeTo(fadeTime,1);
+		} 
+		if($(".image_2").css("opacity")==1) {
+		  	$(".image_2").fadeTo(fadeTime,0);
+		  	$(".image_3").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_3").css("opacity")==1) {
+		  	$(".image_3").fadeTo(fadeTime,0);
+		  	$(".image_4").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_4").css("opacity")==1) {
+		  	$(".image_4").fadeTo(fadeTime,0);
+		  	$(".image_5").fadeTo(fadeTime,1);
+	  	}
+	  	if($(".image_5").css("opacity")==1) {
+		  	$(".image_5").fadeTo(fadeTime,0);
+		  	$(".image_6").fadeTo(fadeTime,1);
+	  	}  
+	  	if($(".image_6").css("opacity")==1) {
+		  	$(".image_6").fadeTo(fadeTime,0);
+		  	$(".image_7").fadeTo(fadeTime,1);
+	  	} 
+	  	if($(".image_7").css("opacity")==1) {
+		  	$(".image_7").fadeTo(fadeTime,0);
+		  	$(".image_8").fadeTo(fadeTime,1);
+	  	}  	
+	  	if($(".image_8").css("opacity")==1) {
+		  	$(".image_8").fadeTo(fadeTime,0);
+		  	$(".image_9").fadeTo(fadeTime,1);
+	  	}  	
+	   	if($(".image_9").css("opacity")==1) {
+		  	$(".image_9").fadeTo(fadeTime,0);
+		  	$(".image_10").fadeTo(fadeTime,1);
+	  	}  	
+	   	if($(".image_10").css("opacity")==1) {
+		  	$(".image_10").fadeTo(fadeTime,0);
+		  	$(".image_1").fadeTo(fadeTime,1);
+	  	}  	
+    }   
+});
+
 		
 // Nav fadein/fadeout
 $(".left_nav").mouseover(function(){
